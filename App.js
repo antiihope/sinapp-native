2575c05ed06c5cd8ed34695d80f60efdbbb4541d
hazem223 <looza.zooma@gmail.com>
2022-12-23 20:00:20 +0200

    Expo app unjected

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { styles } from './src/style';
+import AppHome from './src/index';
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <AppHome />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Text, View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { styles } from './style';
+
+import { useState } from 'react';
+
+const NullSpace = ({ padding }) => {
+  return (
+    <View
+      style={{
+        paddingTop: padding,
+      }}
+    ></View>
+  );
+};
+
+const SingularityLogo = require('../assets/Logo.png');
+
+export default function AppHome() {
+  const [Name, setName] = useState('');
+  return (
+    <View style={styles.container}>
+      <NullSpace padding={10} />
+
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Singularity University
+          <Text>{Name ? `\n\nHello ${Name} !` : ''}</Text>
+        </Text>
+
+        <View style={styles.ImageContainer}>
+          <Image source={SingularityLogo} style={styles.Image} />
+        </View>
+
+        {/*  */}
+
+        <Text style={styles.paragraph}>
+          {'\n\n'}
+          Singularity is a global community{'\n'}
+          using exponential technologies to solve humanity's biggest challenges.
+          {'\n\n'}
+          We are a community of individuals,{'\n'}
+          companies, and organizations{'\n'}
+          who believe that exponential technologies{'\n'}
+          are the key to solving humanity's biggest challenges.
+        </Text>
+
+        {/*  */}
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          onChangeText={(text) => setName(text)}
+          value={Name}
+        />
+
+        <TouchableOpacity
+          style={styles.materialButtonPrimary}
+          onPress={() => {
+            console.log('Pressed');
+          }}
+        >
+          <Text style={styles.Text}>Send</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
